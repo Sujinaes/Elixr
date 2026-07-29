ea4e9e9cb759e46c3becd06000a0959130abca6b
Sujina <sujinaes@gmail.com>
2026-07-29 11:40:24 +0530

    abstraction

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        {
+            Scanner sc = new Scanner(System.in);
+            Shape s;
+            System.out.println(" --------- Area of the Shapes---------");
+            System.out.println(" 1. Area of a Circle ");
+            System.out.println(" 2. Area of the rectangle ");
+            int choice = sc.nextInt();
+            switch(choice){
+                case 1 :
+                    s = new Circle();
+                    s.calculatearea();
+                    break;
+                case 2 :
+                    s= new Rectangle();
+                    s.calculatearea();
+                    break;
+                default:
+                    System.out.println("Invalid Choice");
+
+            }
+            sc.close();
+
+
+        }
+
+    }
+}
